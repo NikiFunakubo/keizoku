292a7fe109861d4a7e1c136acefbfb777ffc43fb
NikiFunakubo <niki.funakubo@gmail.com>
2020-12-30 23:18:49 +0900

    feat:ログインユーザによるいいね押下で黒→赤にアイコン変化 #8[WIP]

---
+++ backend/resources/js/components/ProjectLike.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+        <span class="right floated">10</span>
+        <i class="right floated heart icon" :class="{'inverted red heart icon right floated':this.isLikedBy}"></i>
+
+</div>
+</template>
+<script>
+export default {
+    props: {
+        initialIsLikedBy: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            isLikedBy: this.initialIsLikedBy,
+        }
+    },
+}
+</script>
